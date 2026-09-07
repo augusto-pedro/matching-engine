2f99562c5b66b523d9fe2eebad9f7a768f4a4eaf
Augusto-Pedro <pedroacs157@gmail.com>
2026-09-06 22:32:48 -0300

    match_limit_order() virou match_order() e adição de submit_market_order()

--- include/matching_engine.hpp
+++ include/matching_engine.hpp
@@ -13,12 +13,14 @@ private:
     OrderBook book;
     unsigned long long next_order_id, next_priority;
 
-    std::vector<Trade> match_limit_order(OrderNode *incoming);  // tenta executar a ordem contra as ordens do lado oposto, essas execuções são guardadas em um vetor de Trade
+    std::vector<Trade> match_order(OrderNode *incoming);  // tenta executar a ordem contra as ordens do lado oposto, essas execuções são guardadas em um vetor de Trade
 
 public:
     MatchingEngine();
     
     SubmissionResult submit_limit_order(Side side, int price, int quantity);  // esse método cria uma ordem, tenta executá-la contra o book e, se sobrar quantidade, coloca o restante no book (além de retornar o ID da ordem e a lista de trades que ela gerou)
+
+    std::vector<Trade> submit_market_order(Side side, int quantity);  // tenta executar a ordem contra o book e, se sobrar quantidade ou não, a destrói em seguida
 
     void print_book();
 };
