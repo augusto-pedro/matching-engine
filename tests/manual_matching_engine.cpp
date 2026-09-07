28aabf592b1a55bc1874a0219563eb802ffb8af8
Augusto-Pedro <pedroacs157@gmail.com>
2026-09-06 22:44:27 -0300

    realização de alguns testes com o matching engine

--- tests/manual_matching_engine.cpp
+++ tests/manual_matching_engine.cpp
@@ -1,64 +1,129 @@
 #include <cassert>
 #include <iostream>
+#include <vector>
 
 #include "matching_engine.hpp"
 
 int main()
 {
-    MatchingEngine engine;
+    // TEST 1 - Limit matching
+    {
+        MatchingEngine engine;
 
-    // BUY fica no livro
-    SubmissionResult buy = engine.submit_limit_order(
-        Side::Buy,
-        1000,
-        100
-    );
+        SubmissionResult buy = engine.submit_limit_order(
+            Side::Buy,
+            1000,
+            100
+        );
 
-    assert(buy.order_id == 1);
-    assert(buy.trades.empty());
+        assert(buy.order_id == 1);
+        assert(buy.trades.empty());
 
-    // SELL a 20 também fica no livro
-    SubmissionResult sell1 = engine.submit_limit_order(
-        Side::Sell,
-        2000,
-        100
-    );
+        SubmissionResult sell1 = engine.submit_limit_order(
+            Side::Sell,
+            2000,
+            100
+        );
 
-    assert(sell1.order_id == 2);
-    assert(sell1.trades.empty());
+        assert(sell1.order_id == 2);
+        assert(sell1.trades.empty());
 
-    // Segunda SELL no mesmo preço
-    SubmissionResult sell2 = engine.submit_limit_order(
-        Side::Sell,
-        2000,
-        200
-    );
+        SubmissionResult sell2 = engine.submit_limit_order(
+            Side::Sell,
+            2000,
+            200
+        );
 
-    assert(sell2.order_id == 3);
-    assert(sell2.trades.empty());
+        assert(sell2.order_id == 3);
+        assert(sell2.trades.empty());
 
-    // Essa BUY cruza as duas sells
-    SubmissionResult aggressive_buy = engine.submit_limit_order(
-        Side::Buy,
-        2000,
-        150
-    );
+        SubmissionResult aggressive_buy = engine.submit_limit_order(
+            Side::Buy,
+            2000,
+            150
+        );
 
-    assert(aggressive_buy.trades.size() == 2);
+        assert(aggressive_buy.trades.size() == 2);
 
-    // Primeiro consome order_2 inteira
-    assert(aggressive_buy.trades[0].price == 2000);
-    assert(aggressive_buy.trades[0].quantity == 100);
-    assert(aggressive_buy.trades[0].sell_order_id == 2);
+        assert(aggressive_buy.trades[0].price == 2000);
+        assert(aggressive_buy.trades[0].quantity == 100);
+        assert(aggressive_buy.trades[0].sell_order_id == 2);
 
-    // Depois consome 50 da order_3
-    assert(aggressive_buy.trades[1].price == 2000);
-    assert(aggressive_buy.trades[1].quantity == 50);
-    assert(aggressive_buy.trades[1].sell_order_id == 3);
+        assert(aggressive_buy.trades[1].price == 2000);
+        assert(aggressive_buy.trades[1].quantity == 50);
+        assert(aggressive_buy.trades[1].sell_order_id == 3);
 
-    engine.print_book();
+        engine.print_book();
 
-    std::cout << "\nLimit matching test passed!\n";
+        std::cout << "\nLimit matching test passed!\n";
+    }
+
+
+    // TEST 2 - Market matching
+    {
+        MatchingEngine engine;
+
+        engine.submit_limit_order(
+            Side::Buy,
+            1000,
+            100
+        );
+
+        engine.submit_limit_order(
+            Side::Sell,
+            2000,
+            100
+        );
+
+        engine.submit_limit_order(
+            Side::Sell,
+            2000,
+            200
+        );
+
+        std::vector<Trade> trades1 =
+            engine.submit_market_order(
+                Side::Buy,
+                150
+            );
+
+        assert(trades1.size() == 2);
+
+        assert(trades1[0].price == 2000);
+        assert(trades1[0].quantity == 100);
+
+        assert(trades1[1].price == 2000);
+        assert(trades1[1].quantity == 50);
+
+        // Restam 150 unidades de venda @ 20
+        std::vector<Trade> trades2 =
+            engine.submit_market_order(
+                Side::Buy,
+                200
+            );
+
+        assert(trades2.size() == 1);
+        assert(trades2[0].price == 2000);
+        assert(trades2[0].quantity == 150);
+
+        // As 50 restantes da market buy são descartadas.
+
+        std::vector<Trade> trades3 =
+            engine.submit_market_order(
+                Side::Sell,
+                200
+            );
+
+        assert(trades3.size() == 1);
+        assert(trades3[0].price == 1000);
+        assert(trades3[0].quantity == 100);
+
+        engine.print_book();
+
+        std::cout << "\nMarket order test passed!\n";
+    }
+
+    std::cout << "\nAll MatchingEngine tests passed!\n";
 
     return 0;
 }
